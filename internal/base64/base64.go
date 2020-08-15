2936ad88befd77bb4a75d0222797a8a07b42d1f2
luxarts <bacelolucas@gmail.com>
2020-08-15 04:32:23 -0300

    Moved base64 from utils to own package

---
+++ internal/base64/base64.go
@@ -0,0 +1,21 @@
+package utils
+
+import "encoding/base64"
+
+func Base64EncodeString(data string) string {
+	return base64.URLEncoding.EncodeToString([]byte(data))
+}
+
+func Base64DecodeString(encoded string) string {
+	data, _ := base64.URLEncoding.DecodeString(encoded)
+	return string(data)
+}
+
+func Base64Encode(data []byte) string {
+	return base64.URLEncoding.EncodeToString(data)
+}
+
+func Base64Decode(encoded string) []byte {
+	data, _ := base64.URLEncoding.DecodeString(encoded)
+	return data
+}
--- internal/base64/base64_test.go
+++ internal/base64/base64_test.go
@@ -6,13 +6,13 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_base64_encode(t *testing.T) {
+func Test_Base64EncodeString(t *testing.T) {
 	out := Base64Encode("test1234")
 
 	require.Equal(t, "dGVzdDEyMzQ=", out)
 }
 
-func Test_base64_decode(t *testing.T) {
+func Test_Base64DecodeString(t *testing.T) {
 	out := Base64Decode("dGVzdDEyMzQ=")
 
 	require.Equal(t, "test1234", out)
