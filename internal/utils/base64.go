5888771a9cbed168e0e5e8ea3fb4a32882b0d8ca
luxarts <bacelolucas@gmail.com>
2020-08-15 03:49:08 -0300

    Changed from standard to url encoding/decoding for basse64

--- internal/utils/base64.go
+++ internal/utils/base64.go
@@ -3,10 +3,10 @@ package utils
 import "encoding/base64"
 
 func Base64Encode(data string) string {
-	return base64.StdEncoding.EncodeToString([]byte(data))
+	return base64.URLEncoding.EncodeToString([]byte(data))
 }
 
 func Base64Decode(encoded string) string {
-	data, _ := base64.StdEncoding.DecodeString(encoded)
+	data, _ := base64.URLEncoding.DecodeString(encoded)
 	return string(data)
 }
