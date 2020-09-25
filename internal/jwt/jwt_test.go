af23c9451873f37b0c0bfce55616d8a0376bea64
LuxARTS <bacelolucas@gmail.com>
2020-09-24 21:29:36 -0300

    Tests fixed

--- internal/jwt/jwt_test.go
+++ internal/jwt/jwt_test.go
@@ -10,6 +10,7 @@ import (
 )
 
 func Test_GenerateToken(t *testing.T) {
+	setupTest()
 	header := Header{
 		Algorithm: "HS256",
 		Type:      "JWT",
@@ -21,34 +22,41 @@ func Test_GenerateToken(t *testing.T) {
 	token := GenerateToken(header, payload)
 
 	require.Equal(t, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJDb3NtZSBGdWxhbml0byJ9.LHyGHUaNRnctIkDvGuy2Mr5CAhYwpJ_G8U-F9ccoOJA", token)
+	teardownTest()
 }
 func Test_generateSignature(t *testing.T) {
+	setupTest()
 	header := `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9`
 	payload := `eyJzdWIiOiJDb3NtZSBGdWxhbml0byJ9`
 
 	signature := generateSignature(header, payload)
 
 	require.Equal(t, "LHyGHUaNRnctIkDvGuy2Mr5CAhYwpJ_G8U-F9ccoOJA", signature)
+	teardownTest()
 }
 
 func Test_VerifyOK(t *testing.T) {
+	setupTest()
 	jwt := strings.Split("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJDb3NtZSBGdWxhbml0byJ9.LHyGHUaNRnctIkDvGuy2Mr5CAhYwpJ_G8U-F9ccoOJA", ".")
 
 	out := Verify(jwt[0], jwt[1], jwt[2])
 
 	require.True(t, out)
+	teardownTest()
 }
 
 func Test_VerifyFail(t *testing.T) {
+	setupTest()
 	jwt := strings.Split("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJDb3NtZSBGdWxhbml0byJ8.LHyGHUaNRnctIkDvGuy2Mr5CAhYwpJ_G8U-F9ccoOJA", ".")
 
 	out := Verify(jwt[0], jwt[1], jwt[2])
 
 	require.False(t, out)
+	teardownTest()
 }
 
 func setupTest() {
-	os.Setenv(defines.EnvJWTSecret, "secret")
+	os.Setenv(defines.EnvJWTSecret, "c2VjcmV0")
 }
 func teardownTest() {
 	os.Unsetenv(defines.EnvJWTSecret)
